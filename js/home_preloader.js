8239adf5a7be5aee441730709b944b3da07cb1dc
Munsu kim <jms9301@naver.com>
2019-11-14 10:51:13 +0900

    im loaded and prelaoder

--- js/home_preloader.js
+++ js/home_preloader.js
@@ -2,7 +2,7 @@ const preloader = document.getElementById("js-preloader");
 
 function silder() {
     console.log(preloader);
-    preloader.classList.add("test");
+   
 }
 
 const homeImgArray = [];
@@ -12,10 +12,11 @@ function imageLoad() {
     homeImgArray[0].src = "img/main_logo.gif";
     homeImgArray[1] = new Image();
     homeImgArray[1].src = "img/work_latup_m.gif";
+
     console.log(homeImgArray[0]);
 
     if(homeImgArray[0]){
-        console.log(1);
+        preloader.classList.add("test");
     }
 }
 
