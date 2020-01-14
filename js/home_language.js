42a54f4969faa0709c42ec9351629b45cbaae1c9
Munsu kim <jms9301@naver.com>
2020-01-14 16:59:09 +0900

    lang btn pos

--- js/home_language.js
+++ js/home_language.js
@@ -32,7 +32,7 @@ function langClick() {
         lang.style.fontWeight = "400";
         lang.style.lineHeight = "36px";
         lang.style.letterSpacing = "-0.2px";
-        lang.innerHTML = "안녕하세요. UX 디자이너 김문수입니다. 메터리얼 앤 솔루션(Material and Solution)은 디자인 프로세스의 재료(Material)들을 사용자들을 위한 디자인(Solution)으로 만들어나가는 과정을 기록한 디자인 포트폴리오 입니다.";
+        lang.innerHTML = "안녕하세요. UX 디자이너 김문수입니다. 메터리얼 앤 솔루션(Material and Solution)은 디자인 프로세스의 재료(Material)들을 수집하고 그것을 사용자들을 위한 디자인(Solution)으로 만들어나가는 과정을 기록한 포트폴리오 입니다.";
         setTimeout(() => {
             make_text.innerHTML = "EN";
         }, 500);
@@ -45,8 +45,8 @@ function makeLangBtn() {
     make_btn.style.height = "48px";
     make_btn.style.borderRadius = "50%";
     make_btn.style.backgroundColor = "rgba(47,117,255,0.6)";
-    make_btn.style.bottom = "48px";
-    make_btn.style.right = "48px";
+    make_btn.style.bottom = "72px";
+    make_btn.style.right = "72px";
     make_btn.style.position = "fixed";
     make_btn.style.transition = "all 750ms cubic-bezier(0.455, 0.030, 0.515, 0.955)";
     bodyLang_btn.appendChild(make_btn);
@@ -58,8 +58,8 @@ function makeLangBtn() {
     make_text.style.height = "48px";
     make_text.style.color = "white"
     make_text.style.position = "fixed";
-    make_text.style.bottom = "48px";
-    make_text.style.right = "48px";
+    make_text.style.bottom = "72px";
+    make_text.style.right = "72px";
     make_text.innerHTML = "KR";
     make_text.style.fontFamily = "'Open Sans', sans-serif";
     make_text.style.fontWeight = "400";
--- js/work_language.js
+++ js/work_language.js
@@ -49,8 +49,8 @@ function makeLangBtn() {
     make_btn.style.height = "48px";
     make_btn.style.borderRadius = "50%";
     make_btn.style.backgroundColor = "rgba(47,117,255,0.6)";
-    make_btn.style.bottom = "48px";
-    make_btn.style.right = "48px";
+    make_btn.style.bottom = "72px";
+    make_btn.style.right = "72px";
     make_btn.style.position = "fixed";
     make_btn.style.transition = "all 750ms cubic-bezier(0.455, 0.030, 0.515, 0.955)";
     bodyLang_btn.appendChild(make_btn);
@@ -62,8 +62,8 @@ function makeLangBtn() {
     make_text.style.height = "48px";
     make_text.style.color = "white"
     make_text.style.position = "fixed";
-    make_text.style.bottom = "48px";
-    make_text.style.right = "48px";
+    make_text.style.bottom = "72px";
+    make_text.style.right = "72px";
     make_text.innerHTML = "KR";
     make_text.style.fontFamily = "'Open Sans', sans-serif";
     make_text.style.fontWeight = "400";
