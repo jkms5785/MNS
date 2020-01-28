6fe096bf9376d48606ac3c96d09151db6d9dc377
Munsu kim <jms9301@naver.com>
2020-01-28 11:11:34 +0900

    animation transition

--- js/resume.js
+++ js/resume.js
@@ -148,10 +148,10 @@ function makeResumeModal() {
     makeModal.style.padding = "40px 0";
     makeModal.style.zIndex = "9990";
 
-    makeModal.style.webkitTransition = "all 200ms ease-in-out";
-    makeModal.style.mozTransition = "all 200ms ease-out";
-    makeModal.style.oTransition = "all 200ms ease-out";
-    makeModal.style.transition = "all 200ms ease-out";
+    makeModal.style.webkitTransition = "all 100ms ease-in-out";
+    makeModal.style.mozTransition = "all 100ms ease-out";
+    makeModal.style.oTransition = "all 100ms ease-out";
+    makeModal.style.transition = "all 100ms ease-out";
 
     makeModal.style.webkitTransitionTimingFunction = "ease-out";
     makeModal.style.mozTransitionTimingFunction = "ease-out";
