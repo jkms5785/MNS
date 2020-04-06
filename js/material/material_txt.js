76fbe16e2b63653c09bb4cbbe908ad93a92f313f
Munsu kim <jms9301@naver.com>
2020-04-06 15:21:57 +0900

    txt scroll bubbling fixed

--- js/material/material_txt.js
+++ js/material/material_txt.js
@@ -10,21 +10,25 @@ const txt_tg_1 = document.querySelectorAll(".js-txt-tg_1"),
 let j = 0;
 let z = 0;
 let currentY;
+let fireTxt_1 = true,
+    fireTxt_2 = true;
 
 function txtUp_2() {
+    fireTxt_2 = false;
     if (j < txt_tg_2.length) {
         setTimeout(function () {
             txt_tg_2[j].classList.remove("txt_before");
             txt_tg_2[j].classList.add("txt_after");
             j++;
             txtUp_2(j);
-        }, 100);
+        }, 24);
     } else if (j === txt_tg_2.length) {
         j = 0;
     }
 }
 
 function txtUp_1() {
+    fireTxt_1 = false;
     if (z < txt_tg_1.length) {
         setTimeout(function () {
             txt_tg_1[z].classList.remove("txt_before");
@@ -40,28 +44,42 @@ function txtUp_1() {
 function txt_Que(e) {
     e.preventDefault();
     currentY = window.scrollY;
-    if (currentY >= trg_2.offsetTop - 400 && currentY <= trg_2.offsetTop + trg_2.offsetHeight - 400) {
-        txtUp_2();
-    } else if (currentY <= trg_2.offsetTop - 200 || currentY >= trg_2.offsetTop + trg_2.offsetHeight - 200) {
-        txtReset_2();
+
+    if (fireTxt_1 == true) {
+        if (currentY >= trg_1.offsetTop - 400 && currentY <= trg_1.offsetTop + trg_1.offsetHeight - 400) {
+            z = 0;
+            txtUp_1();
+        }
+    } else if (fireTxt_1 == false) {
+        if (currentY <= trg_1.offsetTop - 400 || currentY >= trg_1.offsetTop + trg_1.offsetHeight - 400) {
+            txtReset_1();
+        }
     }
 
-    if (currentY >= trg_1.offsetTop - 400 && currentY <= trg_1.offsetTop + trg_1.offsetHeight - 400) {
-        txtUp_1();
-    } else if (currentY <= trg_1.offsetTop - 200 || currentY >= trg_1.offsetTop + trg_1.offsetHeight - 200) {
-        txtReset_1();
+    if (fireTxt_2 == true) {
+        if (currentY >= trg_2.offsetTop - 400 && currentY <= trg_2.offsetTop + trg_2.offsetHeight - 400) {
+            j = 0;
+            txtUp_2();
+        }
+    } else if (fireTxt_2 == false) {
+        if (currentY <= trg_2.offsetTop - 200 || currentY >= trg_2.offsetTop + trg_2.offsetHeight - 200) {
+            txtReset_2();
+        }
     }
 }
 
 function txtReset_2() {
+    fireTxt_2 = true;
     let i = 0;
     for (i = 0; i < txt_tg_2.length; i++) {
         txt_tg_2[i].classList.remove("txt_after");
         txt_tg_2[i].classList.add("txt_before");
     }
+    // console.log(`txt2_reset`);
 }
 
 function txtReset_1() {
+    fireTxt_1 = true;
     let i = 0;
     for (i = 0; i < txt_tg_1.length; i++) {
         txt_tg_1[i].classList.remove("txt_after");
