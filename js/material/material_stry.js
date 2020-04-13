8e86ef6d6890d72cd197bb2d27f3a46f7c46b421
Munsu kim <jms9301@naver.com>
2020-04-13 17:10:15 +0900

    MnS story

--- js/material/material_stry.js
+++ js/material/material_stry.js
@@ -1,63 +1,119 @@
-const stry_txt = document.querySelector("#js-stry-txt");
+const stry_txt_1 = document.querySelector("#js-stry-txt-1"),
+    stry_txt_2 = document.querySelector("#js-stry-txt-2");
 
-const random_txt = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
+const ran_txt = [`a`, `!`, `b`, `#`, `3`, `j`, `d`, `i`, `e`, `$`, `i`, `*`];
 
-const M_arry = [`Process`, `Research`, `Interview`, `Persona`, `sketch`, `Prototype`, `User Test`];
+const M_arry = [`Process.`, `Background.`, `Research.`, `Subject.`, `Sketch.`, `Prototype.`, `Testing.`];
 
-const M_txt = [];
+const S_arry = [`Design.`, `Product.`, `Service.`, `Experience.`, `Development.`, `Purpose.`, `Business.`];
 
-// let k = 0;
-const txt_a_z = (a) => {
-    setTimeout(() => {
-        if (a < random_txt.length) {
-            a++;
-            txt_a_z(a);
-            return console.log(random_txt[a]);
+const make_txt = (txt, c) => {
+    const make_span = document.createElement(`span`);
+    let i = 0;
+    setInterval(() => {
+        if (i < ran_txt.length - 1) {
+            i++;
+            make_span.innerHTML = `${ran_txt[i]}`;
+            make_span.style.setProperty(`opacity`, `0.8`);
         } else {
-            a = 0;
-            console.log(`end`);
+            clearInterval();
+            make_span.innerHTML = `${txt}`;
+            make_span.style.setProperty(`opacity`, `1.0`);
         }
-    }, 80);
+    }, 72);
+
+    if (c === `Mtxt`) {
+        stry_txt_1.appendChild(make_span);
+    } else {
+        stry_txt_2.appendChild(make_span);
+    }
 }
 
-console.log(txt_a_z(10));
-
-const stry_txt_make = (a) => {
-    const create_txt = document.createElement(`span`);
-    // create_txt.innerHTML = `${txt_a_z}`;
-    stry_txt.appendChild(create_txt);
+const del_txt = (v, n, b, c) => {
+    if (c === `Mtxt`) {
+        setTimeout(() => {
+            if (stry_txt_1.hasChildNodes()) {
+                stry_txt_1.removeChild(stry_txt_1.lastChild);
+                v++;
+                del_txt(v, n, b, c);
+            } else {
+                v = 0;
+                clearTimeout();
+                setTimeout(() => {
+                    if (b === M_arry.length - 1) {
+                        txt_change(0, 0, c);
+                    } else {
+                        txt_change(n, b + 1, c);
+                    }
+                }, 800);
+            }
+        }, 500 - (`${v * 100 > 400 ? 400 : `${v * 100}` }`));
+    } else {
+        setTimeout(() => {
+            if (stry_txt_2.hasChildNodes()) {
+                stry_txt_2.removeChild(stry_txt_2.lastChild);
+                v++;
+                del_txt(v, n, b, c);
+            } else {
+                v = 0;
+                clearTimeout();
+                setTimeout(() => {
+                    if (b === M_arry.length - 1) {
+                        txt_change(0, 0, c);
+                    } else {
+                        txt_change(n, b + 1, c);
+                    }
+                }, 800);
+            }
+        }, 500 - (`${v * 100 > 400 ? 400 : `${v * 100}` }`));
+    }
 }
 
-let s = 0;
+const txt_change = (n, b, c) => {
+    // n = 0 
+    // b = 0
+    if (c === `Mtxt`) {
+        setTimeout(() => {
+            if (n < M_arry[b].length) {
+                make_txt(M_arry[b].substring(n, n + 1), c);
+                // txt = M_arry[0].substring(0, 1) = P
+                // txt = M_arry[1].substring(1, 2) = P
+                n++;
+                // n = 1
+                txt_change(n, b, c);
+                // txt_change(1, 0);
+            } else {
+                n = 0;
+                setTimeout(() => {
+                    del_txt(0, n, b, c);
+                }, 2500);
+            }
+        }, 48);
+    } else if (c === `Stxt`) {
+        setTimeout(() => {
+            if (n < S_arry[b].length) {
+                make_txt(S_arry[b].substring(n, n + 1), c);
+                // txt = M_arry[0].substring(0, 1) = P
+                // txt = M_arry[1].substring(1, 2) = P
+                n++;
+                // n = 1
+                txt_change(n, b, c);
+                // txt_change(1, 0);
+            } else {
+                n = 0;
+                setTimeout(() => {
+                    del_txt(0, n, b, c);
+                }, 2500);
+            }
+        }, 48);
+    }
 
-const stry_txt_change = () => {
-    setTimeout(() => {
-        if (s <= M_arry[0].length) {
-            stry_txt_make(s);
-            s++;
-            stry_txt_change(s);
-        } else {
-            s = 0;
-        }
-    }, 80);
 }
-
-stry_txt_change();
 
 function init() {
-    stry_txt.style.setProperty(`opacity`, `0.3`);
-    // stry_txt.style.setProperty(`-webkit-transition`, `all 400ms cubic-bezier(0.200, -0.135, 0.125, 1.500)`);
-    // stry_txt.style.setProperty(`-moz-transition`, `all 400ms cubic-bezier(0.200, -0.135, 0.125, 1.500)`);
-    // stry_txt.style.setProperty(`-o-transition`, `all 400ms cubic-bezier(0.200, -0.135, 0.125, 1.500)`);
-    // stry_txt.style.setProperty(`transition`, `all 400ms cubic-bezier(0.200, -0.135, 0.125, 1.500)`);
-
-    // stry_txt.style.setProperty(`-webkit-transition-timing-function`, `cubic-bezier(0.200, -0.135, 0.125, 1.500)`);
-    // stry_txt.style.setProperty(`-moz-transition-timing-function`, `cubic-bezier(0.200, -0.135, 0.125, 1.500)`);
-    // stry_txt.style.setProperty(`-o-transition-timing-function`, `cubic-bezier(0.200, -0.135, 0.125, 1.500)`);
-    // stry_txt.style.setProperty(`transition-timing-function`, `cubic-bezier(0.200, -0.135, 0.125, 1.500)`);
-
-    setTimeout(function () {
-        stry_txt.style.setProperty(`opacity`, `1.0`);
+    txt_change(0, 0, `Mtxt`);
+    setTimeout(() => {
+        txt_change(0, 0, `Stxt`);
     }, 1000);
 }
 
