b05cb0fbed7116ae05e01a34c58ad6d41140c7f7
Munsu kim <jms9301@naver.com>
2020-05-13 15:36:41 +0900

    gray block

--- js/grayblock/grayblock.js
+++ js/grayblock/grayblock.js
@@ -3,11 +3,17 @@ var sc = document.getElementsByClassName('bt');
 var ov_tg = document.getElementById('ov_tg'),
     ov_tg_Top = ov_tg.offsetTop;
 
+var desk_tg = document.getElementById('desk_tg'),
+    desk_tg_Top = desk_tg.offsetTop;
+
 var rs_tg = document.getElementById('rs_tg'),
     rs_tg_Top = rs_tg.offsetTop;
 
 var dp_tg = document.getElementById('dp_tg'),
     dp_tg_Top = dp_tg.offsetTop;
+
+var co_tg = document.getElementById('co_tg'),
+    co_tg_Top = co_tg.offsetTop;
 
 var ut_tg = document.getElementById('ut_tg'),
     ut_tg_Top = ut_tg.offsetTop;
@@ -25,12 +31,20 @@ function ov_scroll() {
     window.scrollTo(0, ov_tg_Top - 100);
 }
 
+function desk_scroll() {
+    window.scrollTo(0, desk_tg_Top - 124);
+}
+
 function rs_scroll() {
-    window.scrollTo(0, rs_tg_Top - 100);
+    window.scrollTo(0, rs_tg_Top - 148);
 }
 
 function dp_scroll() {
-    window.scrollTo(0, dp_tg_Top - 100);
+    window.scrollTo(0, dp_tg_Top - 128);
+}
+
+function co_scroll() {
+    window.scrollTo(0, co_tg_Top - 64);
 }
 
 function ut_scroll() {
@@ -51,12 +65,14 @@ function fd_scroll() {
 
 function init() {
     sc[0].addEventListener('click', ov_scroll),
-        sc[1].addEventListener('click', rs_scroll),
-        sc[2].addEventListener('click', dp_scroll),
-        sc[3].addEventListener('click', ut_scroll),
-        sc[4].addEventListener('click', di_scroll),
-        sc[5].addEventListener('click', pd_scroll),
-        sc[6].addEventListener('click', fd_scroll);
+        sc[1].addEventListener('click', desk_scroll),
+        sc[2].addEventListener('click', rs_scroll),
+        sc[3].addEventListener('click', dp_scroll),
+        sc[4].addEventListener('click', co_scroll),
+        sc[5].addEventListener('click', ut_scroll),
+        sc[6].addEventListener('click', di_scroll),
+        sc[7].addEventListener('click', pd_scroll),
+        sc[8].addEventListener('click', fd_scroll);
 
     var si = document.getElementsByClassName('sc_des');
 
--- js/muto/muto.js
+++ js/muto/muto.js
@@ -18,47 +18,55 @@ var di_tg = document.getElementById('di_tg'),
 var pd_tg = document.getElementById('pd_tg'),
     pd_tg_Top = pd_tg.offsetTop;
 
+var de_tg = document.getElementById('de_tg'),
+    de_tg_Top = de_tg.offsetTop;
+
 var fd_tg = document.getElementById('fd_tg'),
     fd_tg_Top = fd_tg.offsetTop;
 
 function ov_scroll() {
-    window.scrollTo(0, ov_tg_Top - 100);
+    window.scrollTo(0, ov_tg_Top - 64);
 }
 
 function rs_scroll() {
-    window.scrollTo(0, rs_tg_Top - 100);
+    window.scrollTo(0, rs_tg_Top + 560);
 }
 
 function dp_scroll() {
-    window.scrollTo(0, dp_tg_Top - 100);
+    window.scrollTo(0, dp_tg_Top - 40);
 }
 
 function ut_scroll() {
-    window.scrollTo(0, ut_tg_Top - 100);
+    window.scrollTo(0, ut_tg_Top + 32);
 }
 
 function di_scroll() {
-    window.scrollTo(0, di_tg_Top - 100);
+    window.scrollTo(0, di_tg_Top - 80);
 }
 
 function pd_scroll() {
-    window.scrollTo(0, pd_tg_Top - 100);
+    window.scrollTo(0, pd_tg_Top - 120);
+}
+
+function de_scroll() {
+    window.scrollTo(0, de_tg_Top - 100);
 }
 
 function fd_scroll() {
-    window.scrollTo(0, fd_tg_Top - 100);
+    window.scrollTo(0, fd_tg_Top - 40);
 }
 
 var si = document.getElementsByClassName('sc_des');
 
 function init() {
-        sc[0].addEventListener('click', ov_scroll),
+    sc[0].addEventListener('click', ov_scroll),
         sc[1].addEventListener('click', rs_scroll),
         sc[2].addEventListener('click', dp_scroll),
         sc[3].addEventListener('click', ut_scroll),
         sc[4].addEventListener('click', di_scroll),
         sc[5].addEventListener('click', pd_scroll),
-        sc[6].addEventListener('click', fd_scroll);
+        sc[6].addEventListener('click', de_scroll),
+        sc[7].addEventListener('click', fd_scroll);
 
     function li_click(idx) {
         sc[idx].onmouseover = function () {
